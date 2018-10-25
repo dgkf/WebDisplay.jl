7a422ec68ac7c2c03929ed5d116bb344336e72a4
ylxdzsw <ylxdzsw@gmail.com>
2018-10-25 11:33:39 +0800

    reduce info

--- src/WebDisplay.jl
+++ src/WebDisplay.jl
@@ -5,7 +5,9 @@ module WebDisplay
     function __init__()
         host = get(ENV, "WEB_DISPLAY_HOST", "0.0.0.0")
         port = try parse(Int, ENV["WEB_DISPLAY_PORT"]) catch; rand(6000:9000) end
-        @async _web.listen(host, port)
+        @async with_logger(SimpleLogger(stderr, Logging.Warn)) do
+            _web.listen(host, port)
+        end
         pushdisplay(_display)
         @info "WebDisplay listening on $host:$port"
     end
